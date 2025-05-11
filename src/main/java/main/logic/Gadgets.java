1f419b423cf14da870a2bbe5d2dcf94de103e6d5
2141_Dobrovolskii <stimter12@gmail.com>
2025-05-11 04:12:23 +0300

    1

--- src/main/java/main/logic/Gadgets.java
+++ src/main/java/main/logic/Gadgets.java
@@ -1,10 +1,8 @@
 package main.logic;
 
-import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import lombok.*;
 
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,7 +13,6 @@ public abstract class Gadgets {
     private String name;
     private String price;
     private String description;
-    private List<Button> buttons;
 
     public abstract void description();
 }
--- src/main/java/main/logic/Phone.java
+++ src/main/java/main/logic/Phone.java
@@ -1,11 +1,7 @@
 package main.logic;
 
-import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import lombok.*;
-import org.kordamp.ikonli.javafx.FontIcon;
-import java.util.ArrayList;
-import java.util.List;
 
 @AllArgsConstructor
 @Getter
@@ -22,12 +18,6 @@ public class Phone extends Gadgets {
     public Phone(){
         this.setName("-");
         this.setPrice("-");
-        this.setButtons(new ArrayList<>());
-        this.setButtons(new ArrayList<>(List.of(
-                new Button("",new FontIcon("fltfal-info-20")),
-                new Button("",new FontIcon("fltfal-edit-20")),
-                new Button("",new FontIcon("fltfal-delete-20"))
-        )));
         this.setImageView(new ImageView("File:Images\\noImageAvailable.png"));
         this.display=new Display();
         this.frontCamera=new FrontCamera();
